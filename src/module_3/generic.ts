b3adb96dff91428ced5df9c604f588d4bd30cdd7
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-05-03 10:15:28 +0600

    Generics in interface

--- src/module_3/generic.ts
+++ src/module_3/generic.ts
@@ -1,45 +1,48 @@
-
 /* ========================= 3.3 Generics =========================  */
 
-// Basic Generic
-type GenericArray<T> = Array<T>
+// Generics in type_____________________
+type GenericArray<T> = Array<T>;
 
-const rollNumbers: GenericArray<number> = [1, 2, 3, 4, 5]
-const citizens: GenericArray<string> = ['riyad', 'rakib', 'ramim']
+const rollNumbers: GenericArray<number> = [1, 2, 3, 4, 5];
+const citizens: GenericArray<string> = ["riyad", "rakib", "ramim"];
 
 // Generic in Tuple
-type GenericTuple<F,S> = [F,S]
+type GenericTuple<F, S> = [F, S];
 
-const someoneG: GenericTuple<string, boolean> = ['Hello', true] 
+const someoneG: GenericTuple<string, boolean> = ["Hello", true];
 
 type UserG = {
-    name: string, age: number
+  name: string;
+  age: number;
+};
+const usersG: GenericTuple<UserG, number> = [{ name: "riyad", age: 23 }, 10000];
+
+// Generics in interface_____________________
+interface Person2<T, U> {
+  name: string;
+  house: T;
+  car: U;
 }
-const usersG: GenericTuple<UserG, number> = [{name: 'riyad', age: 23}, 10000] 
 
+const someOneIG: Person2<string, boolean> = {
+  name: "someOneIG",
+  house: "Hae",
+  car: false,
+};
 
+// Defaule value
+interface Person3<T, U = null> {
+  name: string;
+  house: T;
+  car?: U;
+}
 
+interface HouseInterface {
+  components: string;
+  totalRooms: number;
+}
 
-
-
-
-
-
-
-
-
-
-
-/* ========================= 3.4 Type Assertion =========================  */
-
-/* ========================= 3.5 Type Assertion =========================  */
-
-/* ========================= 3.6 Type Assertion =========================  */
-
-/* ========================= 3.7 Type Assertion =========================  */
-
-/* ========================= 3.8 Type Assertion =========================  */
-
-/* ========================= 3.9 Type Assertion =========================  */
-
-/* ========================= 3.10 Type Assertion =========================  */
+const someOneIG2: Person3<HouseInterface, boolean> = {
+  name: "someOneIG",
+  house: { components: "Bricks", totalRooms: 5 },
+};
