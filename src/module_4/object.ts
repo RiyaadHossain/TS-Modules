e43454320ddaced83d5270679b9b992dbb6c4e4c
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-05-05 06:40:35 +0600

    object

---
+++ src/module_4/object.ts
@@ -0,0 +1,32 @@
+/* ========================= 4.2 Class, Object , Parameter Properties =========================  */
+class Animal {
+  name: string;
+  species: string;
+  sound: string;
+
+  constructor(name: string, species: string, sound: string) {
+    this.name = name;
+    this.species = species;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    return `${this.name} says ${this.sound}`;
+  }
+}
+
+const cat = new Animal("Persian", "cat", "mew mew"); // Creating Instance
+console.log(cat.makeSound);
+
+// To reduce repeatative task use parameter properties
+class Animal2 {
+  constructor(
+    public name: string, // Use 'public' keyword 
+    public species: string,
+    public sound: string
+  ) {}
+
+  makeSound() {
+    return `${this.name} says ${this.sound}`;
+  }
+}
